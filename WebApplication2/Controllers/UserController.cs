0030f6ab239cbb104012a9d7baef7510b379f289
bruce <S380272@sinyi.com.tw>
2025-04-16 09:11:14 +0800

    加入專案檔案。

---
+++ WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (model.Account =="sinyi" && model.Password == "ken")
+            {
+                TempData["Account"] = model.Account;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "帳號或密碼錯誤");
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.Models
+{
+    public class LoginViewModel
+    {
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+    }
+}
